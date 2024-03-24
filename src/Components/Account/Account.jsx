5d52271706d21181ff11f0b44a217a0f18ad4687
nbtmik <127678334+nbtmik@users.noreply.github.com>
2024-03-24 21:53:45 +0530

    Update Account.jsx

--- src/Components/Account/Account.jsx
+++ src/Components/Account/Account.jsx
@@ -74,7 +74,7 @@ const Account =()=>{
                         <Typography variant="h6">You have not made any post</Typography>
                     )}
                 </div>
-                <div className="accountright">
+                {/* <div className="accountright">
                     <Avatar src={user.avatar.url}
                     sx={{height:"8vmax" , width : "8vmax"}}
                      />
@@ -123,7 +123,7 @@ const Account =()=>{
             <div className="DialogBox">
                 <Typography variant="h4">Following</Typography>
                 {
-                    user && user.following.length > 0 ? user.followers.map((follow)=>((
+                    user && user.following.length > 0 ? user.following.map((follow)=>((
                         <User
                     key={follow._id}
                     userId={follow._id}
@@ -134,7 +134,98 @@ const Account =()=>{
                 }
             </div>
             </Dialog>
-                </div>
+                </div> */}
+                <div className="accountright">
+        <Avatar
+          src={user.avatar.url}
+          sx={{ height: "8vmax", width: "8vmax" }}
+        />
+
+        <Typography variant="h5">{user.name}</Typography>
+
+        <div>
+          <button onClick={() => setFollowersToggle(!followersToggle)}>
+            <Typography>Followers</Typography>
+          </button>
+          <Typography>{user.followers.length}</Typography>
+        </div>
+
+        <div>
+          <button onClick={() => setFollowingToggle(!followingToggle)}>
+            <Typography>Following</Typography>
+          </button>
+          <Typography>{user.following.length}</Typography>
+        </div>
+
+        <div>
+          <Typography>Posts</Typography>
+          <Typography>{user.posts.length}</Typography>
+        </div>
+
+        <Button variant="contained" onClick={logoutHandler}>
+          Logout
+        </Button>
+
+        <Link to="/update/profile">Edit Profile</Link>
+        <Link to="/update/password">Change Password</Link>
+
+        <Button
+          variant="text"
+          style={{ color: "red", margin: "2vmax" }}
+          onClick={deleteProfileHandler}
+          disabled={deleteLoading}
+        >
+          Delete My Profile
+        </Button>
+
+        <Dialog
+          open={followersToggle}
+          onClose={() => setFollowersToggle(!followersToggle)}
+        >
+          <div className="DialogBox">
+            <Typography variant="h4">Followers</Typography>
+
+            {user && user.followers.length > 0 ? (
+              user.followers.map((follower) => (
+                <User
+                  key={follower._id}
+                  userId={follower._id}
+                  name={follower.name}
+                  avatar={follower.avatar.url}
+                />
+              ))
+            ) : (
+              <Typography style={{ margin: "2vmax" }}>
+                You have no followers
+              </Typography>
+            )}
+          </div>
+        </Dialog>
+
+        <Dialog
+          open={followingToggle}
+          onClose={() => setFollowingToggle(!followingToggle)}
+        >
+          <div className="DialogBox">
+            <Typography variant="h4">Following</Typography>
+
+            {user && user.following.length > 0 ? (
+              user.following.map((follow) => (
+                <User
+                  key={follow._id}
+                  userId={follow._id}
+                  name={follow.name}
+                  avatar={follow.avatar.url}
+                />
+              ))
+            ) : (
+              <Typography style={{ margin: "2vmax" }}>
+                You're not following anyone
+              </Typography>
+            )}
+          </div>
+        </Dialog>
+      </div>
             </div>
         )
     );
